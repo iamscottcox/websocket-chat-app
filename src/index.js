62ee867548383563069f4bac828ca41055388562
Scott Cox <scottcox89@gmail.com>
2018-11-30 09:58:58 +0000

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from 'redux-saga';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import chat from "./reducers";
+import { handleNewMessage } from './sagas';
+import { initSocket } from './sockets';
+import { username } from './libs/username';
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(chat, applyMiddleware(sagaMiddleware));
+
+const socket = initSocket(username, store.dispatch);
+
+sagaMiddleware.run(handleNewMessage, { socket, username });
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
